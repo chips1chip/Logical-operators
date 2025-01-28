708b09714497ce3b30e39c3201b88db94d274574
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:56:06 +0530

    Update LogicalOperators.java

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -2,23 +2,15 @@ import java.util.Scanner;
 
 public class LogicalOperators {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        System.out.println("relational opeartor");
-        System.out.println(a>b);
-        System.out.println(a==b);
-        System.out.println(a<b);
-        System.out.println(a>b);
-        System.out.println(a>=b);
-        System.out.println(a<=b);
-        System.out.println("logical opeartor");
-        System.out.println(a>b&&a<b);
-        System.out.println(a<b&&a<b);
-        System.out.println(!(a>b));
+        Scanner sc = new Scanner(System.in); // Scanner for input
+        int x = sc.nextInt(); 
+        int y = sc.nextInt();
+        // Logical Operators section
+        System.out.println("Logical Operators:");
+        System.out.println(x > y && x < y); // Logical AND, checks if both conditions are true
+        System.out.println(x < y && x < y); // Logical AND, checks if both conditions are true
+        System.out.println(!(x > y)); // Logical NOT, negates the condition
 
-        
-
+        sc.close(); 
     }
-    
 }
